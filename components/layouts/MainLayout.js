ce3425e1912d9b027218ea6af3bcef542d69610f
engwa <e.ngwa@tarent.de>
2020-03-10 15:36:16 +0100

    testing

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -31,70 +31,70 @@ class MainLayout extends React.Component {
                             <div className="row">
                                 <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                     <div className="footer-box">
-                                        <h3 className="title-bar-footer">{this.props.t('menu.about_us')}</h3>
+                                        <h3 className="title-bar-footer">{this.props.t('menu-about_us')}</h3>
                                         <ul className="recent-post-link">
                                             <li>
-                                                <Link href="/contact"><a>{this.props.t('menu.contact')}</a></Link>
+                                                <Link href="/contact"><a>{this.props.t('menu-contact')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/security"><a>{this.props.t('menu.security')}</a></Link>
+                                                <Link href="/security"><a>{this.props.t('menu-security')}</a></Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                     <div className="footer-box">
-                                        <h3 className="title-bar-footer">menu.service</h3>
+                                        <h3 className="title-bar-footer">{this.props.t('menu-service')}</h3>
                                         <ul className="recent-post-link">
                                             <li>
-                                                <Link href="/services/index"><a>menu.service</a></Link>
+                                                <Link href="/services/index"><a>{this.props.t('menu-service')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/pricesAndFees"><a>menu.prices_and_fees</a></Link>
+                                                <Link href="/pricesAndFees"><a>{this.props.t('menu-prices_and_fees')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/services/transfer"><a>menu.secureDomainTransfer</a></Link>
+                                                <Link href="/services/transfer"><a>{this.props.t('menu-secureDomainTransfer')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/services/sellonline"><a>menu.sellDomainOnline</a></Link>
+                                                <Link href="/services/sellonline"><a>{this.props.t('menu-sellDomainOnline')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/services/buyonline"><a>menu.buyDomainOnline</a></Link>
+                                                <Link href="/services/buyonline"><a>{this.props.t('menu-buyDomainOnline')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/services/buynowlink"><a>menu.buyNowLink</a></Link>
+                                                <Link href="/services/buynowlink"><a>{this.props.t('menu-buyNowLink')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/services/broker"><a>menu.broker</a></Link>
+                                                <Link href="/services/broker"><a>{this.props.t('menu-broker')}</a></Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                     <div className="footer-box">
-                                        <h3 className="title-bar-footer">menu.support</h3>
+                                        <h3 className="title-bar-footer">{this.props.t('menu-support')}</h3>
                                         <ul className="recent-post-link">
                                             <li>
-                                                <Link href="/faq"><a>menu.faq</a></Link>
+                                                <Link href="/faq"><a>{this.props.t('menu-faq')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/terms"><a>menu.ask_a_question</a></Link>
+                                                <Link href="/terms"><a>{this.props.t('menu-ask_a_question')}</a></Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                     <div className="footer-box">
-                                        <h3 className="title-bar-footer">menu.legal</h3>
+                                        <h3 className="title-bar-footer">{this.props.t('menu-legal')}</h3>
                                         <ul className="recent-post-link">
                                             <li>
-                                                <Link href="/terms"><a>menu.terms_and_conditions</a></Link>
+                                                <Link href="/terms"><a>{this.props.t('menu-terms_and_conditions')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/policy"><a>menu.privacy_policy</a></Link>
+                                                <Link href="/policy"><a>{this.props.t('menu-privacy_policy')}</a></Link>
                                             </li>
                                             <li>
-                                                <Link href="/rules"><a>menu.transaction_rules</a></Link>
+                                                <Link href="/rules"><a>{this.props.t('menu-transaction_rules')}</a></Link>
                                             </li>
                                         </ul>
                                     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
+import React from "react";
 import FrontendLayout from "../components/layouts/FrontendLayout";
 import { withTranslation } from "../i18n";
 
 class IndexPage extends React.Component{
     static async getInitialProps() {
-        return { namespacesRequired: ["common","messages"] };
+        return {
+            namespacesRequired: ['common']
+        };
     }
-    render(){ return (<div>{this.props.t("common:menu.about_us")}</div>)}
+    render(){ return (<div>{this.props.t("common:emailConfirmation")}</div>)}
 };
 
 IndexPage.Layout = FrontendLayout;
